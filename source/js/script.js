7172818a1f8c9b0d5f2b79a35d9c7ecb372ef97a
anashi <n_lazareva@mail.ru>
2019-09-13 23:42:49 +0300

    bug fixes

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,16 @@
 'use strict';
 
-var openPopupButton = document.querySelector('.page-header__contact--link');
+var openPopupButton = document.querySelector('.page-header__button');
 var closePopupButton = document.querySelector('.popup__button--close');
 var formPopup = document.querySelector('.popup');
 var overlay = document.querySelector('.overlay');
 var nameInput = document.querySelector('.popup input[name="name"]');
 var phoneInputs = document.querySelectorAll('input[type="tel"]');
+var scrollButton = document.querySelector('.page-header__scroll-button');
+var navItem = document.querySelector('.page-footer__nav');
+var navButton = document.querySelector('.page-footer__block--nav .page-footer__button');
+var addressItem = document.querySelector('.page-footer__address');
+var addressButton = document.querySelector('.page-footer__block--address .page-footer__button');
 
 var phoneMask = {
   mask: '+{7}(000)000-00-00'
@@ -13,6 +18,16 @@ var phoneMask = {
 
 phoneInputs.forEach(function (phone) {
   IMask(phone, phoneMask);
+});
+
+navButton.addEventListener('click', function () {
+  navButton.classList.toggle('page-footer__button--open');
+  navItem.classList.toggle('page-footer__nav--close');
+});
+
+addressButton.addEventListener('click', function () {
+  addressButton.classList.toggle('page-footer__button--open');
+  addressItem.classList.toggle('page-footer__address--close');
 });
 
 openPopupButton.addEventListener('click', function () {
@@ -37,6 +52,12 @@ window.addEventListener('keydown', function (evt) {
     hideOverlay();
     closePopup();
   }
+});
+
+scrollButton.addEventListener('click', function () {
+  document.querySelector('.advantages').scrollIntoView({
+    behavior: 'smooth'
+  });
 });
 
 var showPopup = function () {
